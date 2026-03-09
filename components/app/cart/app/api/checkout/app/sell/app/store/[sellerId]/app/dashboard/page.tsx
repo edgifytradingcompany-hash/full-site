0070dfc050e96f80da169bfa65a2e7cff7542938
edgifytradingcompany-hash <edgifytradingcompany@gmail.com>
2026-03-09 15:47:55 -0600

    Create Seller Dashboard component

---
+++ components/app/cart/app/api/checkout/app/sell/app/store/[sellerId]/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function Dashboard(){
+
+ return (
+  <div className="p-10">
+
+   <h1 className="text-3xl mb-6">Seller Dashboard</h1>
+
+   <a href="/sell">Add Product</a>
+
+  </div>
+ )
+}
