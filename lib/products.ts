6429d2716e7e42b04af3ae9a937e9f846a11a6eb
edgifytradingcompany-hash <edgifytradingcompany@gmail.com>
2026-03-09 14:48:22 -0600

    Add products array with sample product data

---
+++ lib/products.ts
@@ -0,0 +1,16 @@
+export const products = [
+ {
+  id:1,
+  name:"Edgify Hoodie",
+  price:79,
+  image:"/logo.png",
+  description:"Premium street hoodie"
+ },
+ {
+  id:2,
+  name:"Urban Sneakers",
+  price:120,
+  image:"/logo.png",
+  description:"Limited drop sneakers"
+ }
+];
