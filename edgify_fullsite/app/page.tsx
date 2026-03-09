af29c20fec99e67126e15295b0efe5886edd0d65
edgifytradingcompany-hash <edgifytradingcompany@gmail.com>
2026-03-09 14:54:20 -0600

    Update Home component to include ProductGrid

--- edgify_fullsite/app/page.tsx
+++ edgify_fullsite/app/page.tsx
@@ -1,8 +1,14 @@
+import ProductGrid from "../components/ProductGrid";
+
 export default function Home(){
  return (
-  <div className="p-10 text-center">
-    <img src="/logo.png" className="mx-auto w-32" />
-    <h1 className="text-4xl font-bold mt-4">Welcome to Edgify</h1>
+  <div className="p-10">
+   <h1 className="text-4xl font-bold mb-8">
+    Edgify Marketplace
+   </h1>
+
+   <ProductGrid/>
+
   </div>
  );
 }
