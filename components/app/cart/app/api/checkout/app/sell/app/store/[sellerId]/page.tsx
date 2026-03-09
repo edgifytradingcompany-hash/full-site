2f58b6b5b9bb9215c70fe69a052c63c08e3594a5
edgifytradingcompany-hash <edgifytradingcompany@gmail.com>
2026-03-09 15:46:24 -0600

    Create SellerStore component for seller's products

---
+++ components/app/cart/app/api/checkout/app/sell/app/store/[sellerId]/page.tsx
@@ -0,0 +1,12 @@
+export default function SellerStore(){
+
+ return (
+  <div className="p-10">
+
+   <h1 className="text-3xl mb-6">Seller Store</h1>
+
+   <p>Products from this seller will appear here.</p>
+
+  </div>
+ )
+}
