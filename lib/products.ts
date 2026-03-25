44d999acf7cbd1efa50f3cb697364d7383fddc1f
edgifytradingcompany-hash <edgifytradingcompany@gmail.com>
2026-03-25 14:13:40 -0600

    Remove products array from products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -1,16 +1,1 @@
-export const products = [
- {
-  id:1,
-  name:"Edgify Hoodie",
-  price:79,
-  image:"/logo.png",
-  description:"Premium street hoodie"
- },
- {
-  id:2,
-  name:"Urban Sneakers",
-  price:120,
-  image:"/logo.png",
-  description:"Limited drop sneakers"
- }
-];
+
