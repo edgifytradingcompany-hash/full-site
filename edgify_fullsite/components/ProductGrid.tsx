5093f11f7ec84e614b9acedd948af0ac44735114
edgifytradingcompany-hash <edgifytradingcompany@gmail.com>
2026-03-25 13:47:48 -0600

    Fetch products asynchronously in ProductGrid component

--- edgify_fullsite/components/ProductGrid.tsx
+++ edgify_fullsite/components/ProductGrid.tsx
@@ -1,12 +1,14 @@
-import { products } from "../lib/products";
+import { getProducts } from "../lib/api/products";
 import ProductCard from "./ProductCard";
 
-export default function ProductGrid(){
- return (
-  <div className="grid grid-cols-3 gap-6">
-   {products.map(p => (
-    <ProductCard key={p.id} product={p}/>
-   ))}
-  </div>
- );
+export default async function ProductGrid() {
+  const products = await getProducts();
+
+  return (
+    <div className="grid grid-cols-3 gap-6">
+      {products.map((p) => (
+        <ProductCard key={p.id} product={p} />
+      ))}
+    </div>
+  );
 }
